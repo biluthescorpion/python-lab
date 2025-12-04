45424342b97cf79e91b77751326b20fa77d929cd
bilal <biluthescorpional@gmail.com>
2025-12-04 10:12:37 +0500

    Add three Python function files

---
+++ greet_user.py
@@ -0,0 +1,4 @@
+def greet_user():
+    print("Hello User")
+
+greet_user()
---
+++ multiply.py
@@ -0,0 +1,7 @@
+def multiply(a, b):
+	return a*b
+
+a = int(input("Enter your first number: "))
+b = int(input("Enter your second number: "))
+
+print(f"Product: {multiply(a,b)}")
